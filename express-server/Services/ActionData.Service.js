1c0375965d13a8f985f2bb831750f9fd24f4800a
ELMIR.WEB <elmir.web@gmail.com>
2022-08-09 10:14:04 +0500

    все готово

--- express-server/Services/ActionData.Service.js
+++ express-server/Services/ActionData.Service.js
@@ -1,14 +1,41 @@
+const moment = require(`moment`);
+
 class actionDataService {
   async getData(table_name, date_from, date_to) {
-    const [queryRows] = await global.connectMySQL.execute(
-      `SELECT * FROM ${table_name} `
-    );
+    try {
+      const [rowsAllEntities] = await global.connectMySQL.execute(
+        `SELECT * FROM ${table_name} WHERE bdatetime BETWEEN '${moment(
+          date_from
+        ).format('YYYY-MM-DD HH:mm:ss')}' AND '${moment(date_to).format(
+          'YYYY-MM-DD HH:mm:ss'
+        )}'`
+      );
 
-    for (let index = 0; index < queryRows.length; index++) {
-      console.log(JSON.stringify(queryRows[index].bdatetime));
+      if (rowsAllEntities.length === 0)
+        return {
+          error: true,
+          object: {
+            message: 'Нет записей в БД соответствующих данным в JSON.',
+            table_name: `Запрощенная таблица: ${table_name}`,
+            date_from: `Дата от: ${moment(date_from).format(
+              'YYYY-MM-DD HH:mm:ss'
+            )}`,
+            date_to: `Дата до: ${moment(date_to).format(
+              'YYYY-MM-DD HH:mm:ss'
+            )}`,
+          },
+        };
+      else
+        return {
+          error: false,
+          object: { rows: rowsAllEntities },
+        };
+    } catch (errors) {
+      return {
+        error: true,
+        object: errors,
+      };
     }
-
-    return queryRows;
   }
 }
 
